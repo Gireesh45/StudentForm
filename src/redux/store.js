ee6c2b0946024d438497835fa0bbe7242ea7db38
GireeshKumar <kudupudivenkata95@gmail.com>
2025-02-24 17:06:47 +0530

    Removed Commented Code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,3 @@
-// import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-// const studentSlice = createSlice({
-//   name: 'students',
-//   initialState: [],
-//   reducers: {
-//     addStudent: (state, action) => {
-//       state.push(action.payload);
-//     }
-//   }
-// });
-
-// export const { addStudent } = studentSlice.actions;
-
-// const store = configureStore({
-//   reducer: {
-//     students: studentSlice.reducer
-//   }
-// });
-
-// export default store;
-
-
 import { configureStore } from '@reduxjs/toolkit';
 import studentReducer from './studentSlice'; // Import reducer
 
